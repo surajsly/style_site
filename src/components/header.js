aa03855f6412a9b80fa36401b7e30af1ef7b39c3
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-11-17 18:53:29 +0530

    header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,13 @@
 import React from "react"
 
+import styled from "styled-components"
+
+const HeaderNav = styled`
+background:bluevoilet
+`
+
 const Header = () => {
-  return <div>header</div>
+  return <headerNav></headerNav>
 }
 
 export default Header
